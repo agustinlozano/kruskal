60ed543c97d53e5891849976f40a9d1ae326020d
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-12-10 20:16:21 -0300

    fix syntax: init and find functions

--- init.c
+++ init.c
@@ -1,8 +1,8 @@
 #include "kruskal.h"
 
-void init(name_type A, element_type x, CE_set C) {
-  C.names[x].set_name = A;
-  C.names[x].next = 0;
-  C.set_headers[A].count = 1;
-  C.set_headers[A].firts_element = x;
+void init(name_type A, element_type x, CE_set *C) {
+  C->names[x].set_name = A;
+  C->names[x].next = 0;
+  C->set_headers[A].count = 1;
+  C->set_headers[A].firts_element = x;
 }
