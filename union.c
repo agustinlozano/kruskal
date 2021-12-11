bd9aa09771e50e48dce3bcf32e3a66293a0df5c3
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-12-10 21:58:34 -0300

    Edit union file Correct syntax error Correct typo error Fix loop

--- union.c
+++ union.c
@@ -1,39 +1,37 @@
 #include "kruskal.h"
 
-void _union(name_type A, name_type B, CE_set C) {
-  int i;
-
+void _union(name_type A, name_type B, CE_set *C) {
   //A es el conjunto mas grande, combina B dendro de A
-  if (C.set_headers[A].count > C.set_headers[B].count) {
-    i = C.set_headers[B].firts_element;
+  if (C->set_headers[A].count > C->set_headers[B].count) {
+    int i = C->set_headers[B].firt_element;
 
     /* Encuentra el final de B, cambiando los nombres de los conjuntos
      * por A conforme se avanza
      */
-    do {
-      C.names[i].set_name = A;
-      i = C.names[i].next;
-    } while (C.names[i].next == 0);
+    for (int j = 0; j < VERTICES-1; j++) {
+      C->names[i].set_name = A;
+      i = C->names[i].next;
+    }
 
     //Agrega a la lista A al final de la B y llama A al resultado
     //Ahora i es el indice del ultimo elemento de B
-    C.names[i].set_name = A;
-    C.names[i].next = C.set_headers[A].firts_element;
-    C.set_headers[A].firts_element = C.set_headers[B].firts_element;
-    C.set_headers[A].count = C.set_headers[A].count + C.set_headers[B].count;
+    C->names[i].set_name = A;
+    C->names[i].next = C->set_headers[A].firt_element;
+    C->set_headers[A].firt_element = C->set_headers[B].firt_element;
+    C->set_headers[A].count = (C->set_headers[A].count) + (C->set_headers[B].count);
   
   //B es al menos tan grande como A
   } else {
-    i = C.set_headers[A].firts_element;
+    int i = C->set_headers[A].firt_element;
 
-    do {
-      C.names[i].set_name = B;
-      i = C.names[i].next;
-    } while (C.names[i].next == 0);
+    for (int j = 0; j < VERTICES-1; j++) {
+      C->names[i].set_name = A;
+      i = C->names[i].next;
+    }
 
-    C.names[i].set_name = B;
-    C.names[i].next = C.set_headers[B].firts_element;
-    C.set_headers[B].firts_element = C.set_headers[A].firts_element;
-    C.set_headers[B].count = C.set_headers[A].count + C.set_headers[B].count;
+    C->names[i].set_name = B;
+    C->names[i].next = C->set_headers[B].firt_element;
+    C->set_headers[B].firt_element = C->set_headers[A].firt_element;
+    C->set_headers[B].count = (C->set_headers[A].count) + (C->set_headers[B].count);
   }
 }
